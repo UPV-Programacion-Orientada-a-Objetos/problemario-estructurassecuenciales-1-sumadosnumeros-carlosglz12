7e40e353ee466e20698bb635d8c8465bef3136d2
Carlos Bocanegra <2130093@upv.edu.mx>
2023-05-11 12:00:24 -0600

    si jala

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 class Main {
 
     public static void main(String[] args) {
-
+        int a = Integer.parseint(args[0]);
+        int b = Integer.parseint(args[1]);
+        SumaDeDosNUmeros obj = new SumaDosNumeros();
+        System.out.println(obj.suma(a,terb));
     }
 }
